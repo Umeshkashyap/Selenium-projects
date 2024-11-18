078089f2ed4157fa17269fb75677477dc0490f2d
umeshkshyp <133477868+umeshkshyp@users.noreply.github.com>
2024-11-18 11:59:31 +0530

    Handle alert with ok cancle and with textbox

--- demoAutomationTesting/authenthicatedPopUp.py
+++ demoAutomationTesting/authenthicatedPopUp.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
+print()
 serv_obj=Service("C:\\drivers\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe")
 driver=webdriver.Chrome(service=serv_obj)
 
