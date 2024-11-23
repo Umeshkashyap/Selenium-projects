a02058162ad5c52afea0f91c34178ddf97bb821e
umeshkshyp <45291532+Umeshkashyap@users.noreply.github.com>
2024-11-23 23:53:45 +0530

    Perform right click

---
+++ AutomationTesting/mouse operations/right click.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Set up Chrome driver
+serv_obj = Service("C:\\drivers\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+myWait = WebDriverWait(driver, 10)
+
+driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html")
+driver.maximize_window()
+
+button=driver.find_element(By.XPATH,"//span[@class='context-menu-one btn btn-neutral']")
+
+act=ActionChains(driver)
+# context_click() using for right click operation
+act.context_click(button).perform()
+print("clicked")
+
+driver.quit()
