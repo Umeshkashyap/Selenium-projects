6074b881134011132ca53ed759d78c09c45febc2
umeshkshyp <45291532+Umeshkashyap@users.noreply.github.com>
2024-11-23 23:24:17 +0530

    Perform Mouse Hover

--- AutomationTesting/mouse operations/mouse hover.py
+++ AutomationTesting/mouse operations/mouse hover.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -11,6 +12,14 @@ serv_obj = Service("C:\\drivers\\chromedriver-win64 (2)\\chromedriver-win64\\chr
 driver = webdriver.Chrome(service=serv_obj)
 myWait = WebDriverWait(driver, 10)
 
-driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+driver.get("https://testautomationpractice.blogspot.com/")
 driver.maximize_window()
 
+button=driver.find_element(By.XPATH,"//button[normalize-space()='Point Me']")
+firstEle=driver.find_element(By.XPATH,"(//a[normalize-space()='Mobiles'])[1]")
+secondEle=driver.find_element(By.XPATH,"(//a[normalize-space()='Laptops'])[1]")
+act=ActionChains(driver)
+act.move_to_element(button).move_to_element(firstEle).move_to_element(secondEle).click().perform()
+print("Hover succes")
+
+driver.quit()
