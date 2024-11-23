822a5a4756ccad12dddd182a7b78861738f1b41f
umeshkshyp <45291532+Umeshkashyap@users.noreply.github.com>
2024-11-23 23:52:31 +0530

    Perform Double click

---
+++ AutomationTesting/mouse operations/double click.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Set up Chrome driver
+serv_obj = Service("C:\\drivers\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+myWait = WebDriverWait(driver, 10)
+
+driver.get("https://testautomationpractice.blogspot.com/")
+driver.maximize_window()
+
+inpBox=driver.find_element(By.XPATH, "//input[@id='field1']")
+inpBox.clear()
+inpBox.send_keys("Testing")
+
+button=driver.find_element(By.XPATH,"//button[normalize-space()='Copy Text']")
+act=ActionChains(driver)
+
+act.double_click(button).perform()
+time.sleep(5)
