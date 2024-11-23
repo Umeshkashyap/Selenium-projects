7ebd69ec401ab10ce58523db987248b822d6e411
umeshkshyp <45291532+Umeshkashyap@users.noreply.github.com>
2024-11-23 23:09:57 +0530

    Mouse operations

---
+++ AutomationTesting/mouse operations/mouse hover.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Set up Chrome driver
+serv_obj = Service("C:\\drivers\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+myWait = WebDriverWait(driver, 10)
+
+driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+driver.maximize_window()
+
