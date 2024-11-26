5ff3734d956506645760cc7c1f1390a2faa98147
umeshkshyp <45291532+Umeshkashyap@users.noreply.github.com>
2024-11-26 23:49:17 +0530

    screenshot

---
+++ AutomationTesting/screenshot.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
+
+# Set up Chrome driver
+serv_obj = Service("C:\\drivers\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe")
+driver = webdriver.Chrome(service=serv_obj)
+myWait = WebDriverWait(driver, 10)
+
+driver.get("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+driver.maximize_window()
+
+driver.save_screenshot("C:\\Users\\Admin\\PycharmProjects\\Selenium-projects\\AutomationTesting\\homepage.png")
+print("succes")
